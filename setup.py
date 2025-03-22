cb5f9a58dbdabdd1f67cc0bd8f12afedfd96a58f
Anurag Singh <as040999@gmail.com>
2025-03-22 17:24:27 +0000

    added files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "MLPipeline",
+    version= "0.0.0",
+    author="Anurag",
+    author_email="as040999@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+)
